ab9427f3af3d103b64c1ea627f4a54a9b45b7eb5
Sandro Simes <sandro.simes92@gmail.com>
2016-08-28 16:36:53 +0100

    Initial Commit

---
+++ 21-RetroCalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  21-RetroCalculatorApp
+//
+//  Created by Sandro Simes on 28/08/16.
+//  Copyright © 2016 SandroSimes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
