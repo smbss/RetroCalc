5e525577bf7196988f8be08d2dd477fb8304d434
Sandro Simes <sandro.simes92@gmail.com>
2016-08-28 21:36:57 +0100

    Added sound in each number button

--- 21-RetroCalculatorApp/ViewController.swift
+++ 21-RetroCalculatorApp/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+        // Creating the instance that will be used to play the sound
+    var btnSound: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            // Getting the path for the sound
+        let path = Bundle.main.pathForResource("btn", ofType: "wav")
+            // Converting path (String) to an url
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+                // Attaching the sound to the instance created
+            try btnSound = AVAudioPlayer(contentsOf: soundURL)
+                // Anything that "throws" in the method suggestion list needs a "do catch" statement
+            btnSound.prepareToPlay() // Preparing the button to play before we even use it
+        } catch let err as NSError {
+            print(err.debugDescription) // Printing the error in case it fails
+        }
+    
     }
-
+    
+        // Created the action for the pressed button (connected to each number)
+    @IBAction func numberPressed(sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        if btnSound.isPlaying {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
 
 }
-
